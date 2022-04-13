e6d14b5247578c4242773b3b6c789c09a96bf5cd
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-04-13 17:46:31 +0200

    updated the main.js
    updated the main.js by...
    -making some variables global
    -changes some names to include screaming snake case and camel case
    -added more comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+/*
+ ____the naming conventions used____
+    -camelCase = for normal variable and function declaration
+    -SCREAMING_SNAKE_CASE = for global variable and function declaration
+ */
 
 //when page loads, get all tasks remaining from local storage
 window.onload = loadTasks;
@@ -9,53 +14,82 @@ addTask();
 });
 
 //display form when "create task" button is clicked
-function displayModal(){
-    const modal = document.querySelector("#modal")
-    modal.style.display = "block"
+/* ------modal------ */
+
+    /* ---variables--- */
+const MODAL = document.querySelector("#modal")
+const MODAL_BTN = document.querySelector("#modalBtn")
+const CANCEL_BTN = document.querySelector("#cancelBtn")
+
+    /* ---functions--- */
+const displayModal = () => {
+  MODAL.style.display = "block"
 }
 
-//hide modal when "cancel" is clicked
+const hideModal = () => {
+  MODAL.style.display = "none"
+}
+
+    /* event listeners */
+MODAL_BTN.addEventListener('click', displayModal)
+CANCEL_BTN.addEventListener('click', hideModal)
+
+/* function displayModal(){
+    const modal = document.querySelector("#modal")
+    modal.style.display = "block"
+} 
+
 function hideModal(){
     const modal = document.querySelector("#modal")
     modal.style.display = "none"
     document.getElementById("taskMessage").innerHTML = ""
-}
+} */
+
+/* ------ Functionality ------ */
+
+     /* ---variables--- */
+
+// Get the tasks from localStorage and convert it to an array
+const TASKS = Array.from(JSON.parse(localStorage.getItem("tasks")));
+
+      /*  ---functions--- */
 
 function loadTasks() {
     // check if localStorage has any tasks
+
     // if not then return
     if (localStorage.getItem("tasks") == null) return;
 
-    // Get the tasks from localStorage and convert it to an array
-    let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-
     // Loop through the tasks and add them to the list
-    tasks.forEach(task => {
-      const list = document.querySelector("ul");
+    TASKS.forEach(task => {
+      const list = document.querySelector("#toDoList");
       const li = document.createElement("li");
       li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
         <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
         <button class="delBtnContainer" onclick="removeTask(this)"> <img src=".//media/iconmonstr-x-mark-7-240.png" alt="delete img" class="delBtn" > </button>`;
       list.insertBefore(li, list.children[0]);
-    });
+  });
 }
 
-//add task to list
+//add taskInput to list
 function addTask() {
     const task = document.querySelector("form input");
-    const list = document.querySelector("#myUl");
+    const list = document.querySelector("#toDoList");
+    
     // return if task is empty
     if (task.value === "") {
-      document.getElementById("taskMessage").innerHTML ="Please add a task!"
+      document.getElementById("formMessage").innerHTML ="Please add a task!"
       return false;
     }
+    
     // check is task already exist
     if (document.querySelector(`input[value="${task.value}"]`)) {
-      document.getElementById("taskMessage").innerHTML = "This task exists!"
+      document.getElementById("formMessage").innerHTML = "This task exists!"
       return false;
     }
+    
     else{
-      document.getElementById("taskMessage").innerHTML = ""
+      document.getElementById("formMessage").innerHTML = ""
     }
 
     // add task to local storage
@@ -66,42 +100,39 @@ function addTask() {
     li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
     <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
     <button class="delBtnContainer" onclick="removeTask(this)"> <img src=".//media/iconmonstr-x-mark-7-240.png" alt="delete img" class="delBtn" > </button>`;
-    /* or this could work
-     list.insertBefore(li, list.children[0]); 
-     */
     list.appendChild(li)
-    document.getElementById("taskMessage").innerHTML =""
+    document.getElementById("formMessage").innerHTML =""
+    
     // clears input field
     task.value = "";
 }
 
-//checked completed tasks
+//checks off completed tasks
 function taskComplete(event) {
-    let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-    tasks.forEach(task => {
+  TASKS.forEach(task => {
       if (task.task === event.nextElementSibling.value) {
         task.completed = !task.completed;
       }
-
     });
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(TASKS));
     event.nextElementSibling.classList.toggle("completed");
 }
 
+
+//removes task from list and local storage
 function removeTask(event) {
-    let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-    tasks.forEach(task => {
+  TASKS.forEach(task => {
       if (task.task === event.parentNode.children[1].value) {
         // delete task
-        tasks.splice(tasks.indexOf(task), 1);
+        TASKS.splice(TASKS.indexOf(task), 1);
       }
     });
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(TASKS));
     event.parentElement.remove();
 }
 
 // store current task to track changes
-var currentTask = null;
+let currentTask = null;
 
 // get current task
 function getCurrentTask(event) {
@@ -109,35 +140,41 @@ function getCurrentTask(event) {
 }
 
 // edit the task and update local storage
-function editTask(event) {
-    let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+function editTask(event) {    
     // check if task is empty
     if (event.value === "") {
-      document.getElementById("taskMessage2").innerHTML = "The task is empty"
+      document.getElementById("errorMessage").innerHTML = "The task is empty"
       event.value = currentTask;
       return;
     }
+
     else
     {
-      document.getElementById("taskMessage2").innerHTML = ""
+      document.getElementById("errorMessage").innerHTML = ""
     }
-    // task already exist
-    tasks.forEach(task => {
+
+    // if task already exist
+    TASKS.forEach(task => {
       if (task.task === event.value) {
-      document.getElementById("taskMessage2").innerHTML = "The task already exist!";
+      document.getElementById("errorMessage").innerHTML = "The task already exist!";
         event.value = currentTask;
         return;
       }
+
       else{
-      document.getElementById("taskMessage2").innerHTML = "";
+      document.getElementById("errorMessage").innerHTML = "";
       }
+
     });
+    
     // update task
-    tasks.forEach(task => {
+    TASKS.forEach(task => {
       if (task.task === currentTask) {
         task.task = event.value;
       }
+      
     });
+
     // update local storage
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(TASKS));
 }
