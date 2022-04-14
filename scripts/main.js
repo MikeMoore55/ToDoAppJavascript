f1e573e6d70520dab20f5d6a3ff0bf5da4d7928a
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-04-14 08:39:34 +0200

    fixed main.js date and time feature
    added date and time functionality to app

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 /*
  ____the naming conventions used____
+
     -camelCase = for normal variable and function declaration
     -SCREAMING_SNAKE_CASE = for global variable and function declaration
- */
+ 
+*/
 
 //when page loads, get all tasks remaining from local storage
 window.onload = loadTasks;
@@ -13,9 +15,11 @@ e.preventDefault();
 addTask();
 });
 
-//display form when "create task" button is clicked
+
 /* ------modal------ */
 
+//display form/modal when "create task" button is clicked
+    
     /* ---variables--- */
 const MODAL = document.querySelector("#modal")
 const MODAL_BTN = document.querySelector("#modalBtn")
@@ -36,14 +40,15 @@ CANCEL_BTN.addEventListener('click', hideModal)
 
 
 
-/* ------ Functionality ------ */
+/* ------adding tasks Functionality ------ */
 
      /* ---variables--- */
 
 // Get the tasks from localStorage and convert it to an array
 const TASKS = Array.from(JSON.parse(localStorage.getItem("tasks")));
+const LI = document.createElement("li"); 
 
-      /*  ---functions--- */
+     /*  ---functions--- */
 
 function loadTasks() {
     // check if localStorage has any tasks
@@ -54,22 +59,28 @@ function loadTasks() {
     // Loop through the tasks and add them to the list
     TASKS.forEach(task => {
       const list = document.querySelector("#toDoList");
-      const li = document.createElement("li");
-      li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+      
+      LI.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
         <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+        <br>
+        <input type="date" value="${date.date}" class="taskDueDate">
+        <input type="time" value="${time.time}" class="taskDueDate">
         <button class="delBtnContainer" onclick="removeTask(this)"> <img src=".//media/iconmonstr-x-mark-7-240.png" alt="delete img" class="delBtnImg" > </button>`;
-      list.insertBefore(li, list.children[0]);
+      list.insertBefore(LI, list.children[0]);
   });
 }
 
 //add taskInput to list
 function addTask() {
-    const task = document.querySelector("form input");
+    const task = document.querySelector("#taskInput");
     const list = document.querySelector("#toDoList");
     
-    // return if task is empty
+    const taskDate = document.querySelector("#time")
+    const taskTime =  document.querySelector("#date")
+
+    // return false if task is empty
     if (task.value === "") {
-      document.getElementById("formMessage").innerHTML ="Please add a task!"
+      document.getElementById("formMessage").innerHTML ="**This field cannot be left empty!**"
       return false;
     }
     
@@ -84,19 +95,27 @@ function addTask() {
     }
 
     // add task to local storage
-    localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
+    localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false, date: taskDate.value, time: taskTime.value }]));
 
     // create list item, add innerHTML and append to ul
-    const li = document.createElement("li");
-    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
+  
+    LI.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
     <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+    <br>
+    <input type="date" value="${taskDate.value}" class="taskDueDate">
+    <input type="time" value="${taskTime.value}" class="taskDueDate">
     <button class="delBtnContainer" onclick="removeTask(this)"> <img src=".//media/iconmonstr-x-mark-7-240.png" alt="delete img" class="delBtnImg" > </button>`;
-    list.appendChild(li)
-    document.getElementById("formMessage").innerHTML =""
+    list.appendChild(LI)
+
+    document.getElementById("formMessage").innerHTML = ""
     
     // clears input field
     task.value = "";
+    taskDate.value = "";
+    taskTime.value = "";
 }
+
+//create function for form validation
 
 //checks off completed tasks
 function taskComplete(event) {
@@ -116,6 +135,8 @@ function removeTask(event) {
       if (task.task === event.parentNode.children[1].value) {
         // delete task
         TASKS.splice(TASKS.indexOf(task), 1);
+        TASKS.splice(TASKS.indexOf(date), 1);
+        TASKS.splice(TASKS.indexOf(time), 1)
       }
     });
     localStorage.setItem("tasks", JSON.stringify(TASKS));
