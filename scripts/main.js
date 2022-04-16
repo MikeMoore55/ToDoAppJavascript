10882a733c11eb1c621f98a8d402a9a919293b55
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-04-16 16:06:25 +0200

    update version 1 pt2
    updating version one
    
    main.js
    
    so i decided to redo my javascript entirely, i made it more readable, and easier to understand when just scanning through
    
    added console logs, to keep track of what's going on in the console
    
    -new edit function
    -new working time function

--- scripts/main.js
+++ scripts/main.js
@@ -1,217 +1,127 @@
+/* ---- variables ---- */
 
-//when page loads, get all tasks remaining from local storage
-window.onload = loadTasks;
+const modal = document.querySelector("#form");
+const taskInput = document.querySelector("#textInput");
+const dateInput = document.querySelector("#dateInput");
+const taskDescription = document.querySelector("#textarea");
+const msg = document.querySelector("#msg");
+const tasks = document.querySelector("#tasks");
+const add = document.querySelector("#add");
 
-//prevent the task input form from "bugging"
-document.querySelector("form").addEventListener("submit", e => {
-e.preventDefault();
-addTask();
+/* ---- event listener ---- */
+
+modal.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
 });
 
+/* ---- form validation ---- */
 
-/* ----- make modal appear and disappear ----- */
+const formValidation = () => {
+  // if task input field is blank then...
+  if (taskInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "**Task cannot be blank**";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptTask();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
 
-const MODAL = document.querySelector("#modal")
-const MODAL_BTN = document.querySelector("#modalBtn")
-const CANCEL_BTN = document.querySelector("#cancelBtn")
-
-const displayModal = () => {
-  MODAL.style.display = "block"
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
 };
 
-const hideModal = () => {
-  MODAL.style.display = "none"
-};
+// create empty array for task
+// task = object within an array
+
+let task = [{}];
+
+/* ---- functions ---- */
 
 
-/* ----- Get the tasks from localStorage ----- */
-const TASKS = Array.from(JSON.parse(localStorage.getItem("tasks")));
+// set tasks value within the array with certain fields
 
-
-/* ----- load tasks from local storage ----- */
-
-function loadTasks() {
-    // check if localStorage has any tasks
-
-    // if not then return
-    if (localStorage.getItem("tasks") == null) return;
-
-    // Loop through the tasks and add them to the list
-    TASKS.forEach(task => {
-      const list = document.querySelector("#toDoList");
-      const li = document.createElement("li");
-      li.innerHTML = 
-      ` <input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
-        <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-        <i id="delBtn" class="fa fa-times delBtn" aria-hidden="true" onclick="removeTask(this)"></i>`
-        list.insertBefore(li, list.children[0]);
+const acceptTask = () => {
+  task.push({
+    text: taskInput.value,//task name
+    date: dateInput.value,// task due date
+    description: taskDescription.value,// task description
   });
-};
 
-/* ----- add taskInput to list ----- */
+// create local storage item with key = tasks
 
-function addTask() {
-    const task = document.querySelector("form input");
-    const list = document.querySelector("#toDoList");
-    
-    // return if task is empty
-    if (task.value === "") {
-      document.getElementById("formMessage").innerHTML ="Please add a task!"
-      return false;
-    }
-    
-    // check is task already exist
-    if (document.querySelector(`input[value="${task.value}"]`)) {
-      document.getElementById("formMessage").innerHTML = "This task exists!"
-      return false;
-    }
-    
-    else{
-      document.getElementById("formMessage").innerHTML = ""
-    }
+  localStorage.setItem("tasks", JSON.stringify(task));
 
-    // add task to local storage
-    localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
-
-    // create list item, add innerHTML and append to ul
-    const li = document.createElement("li");
-    li.innerHTML = 
-    ` <input type="checkbox" onclick="taskComplete(this)" class="check">
-      <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
-      <i id="delBtn" class="fa fa-times delBtn" aria-hidden="true" onclick="removeTask(this)"></i>
-    `;
-    list.appendChild(li)
-    document.getElementById("formMessage").innerHTML ="";
-    
-    // clears input field
-    task.value = "";
-
-    console.log(TASKS)
+  console.log(task);
+  createTasks();
 };
 
 
-/* ----- checks off completed tasks when check box is clicked ----- */
-function taskComplete(event) {
-  TASKS.forEach(task => {
-      if (task.task === event.nextElementSibling.value) {
-        task.completed = !task.completed;
-      }
-    });
-    localStorage.setItem("tasks", JSON.stringify(TASKS));
-    event.nextElementSibling.classList.toggle("completed");
+// creates a task and displays it
 
-    const containsClass = nextElementSibling.classList.contains("completed")
-    if (containsClass){
-      document.querySelector("#completedList").appendChild(li)
-    }
+const createTasks = () => {
+  tasks.innerHTML = "";
+  task.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+  });
+//when task is added reset the form to default values
+
+  resetForm();
 };
 
 
-/* ----- removes task from list and local storage when x is clicked ----- */
-function removeTask(event) {
-  TASKS.forEach(task => {
-      if (task.task === event.parentNode.children[1].value) {
-        // delete task
-        TASKS.splice(TASKS.indexOf(task), 1);
-      }
-    });
-    localStorage.setItem("tasks", JSON.stringify(TASKS));
-    event.parentElement.remove();
-};
+//delete tasks
 
-// store current task to track changes
-let currentTask = null;
-
-// get current task
-function getCurrentTask(event) {
-    currentTask = event.value;
-};
-
-/* ----- edit the task and update local storage ----- */
-function editTask(event) {    
-    // check if task is empty
-    if (event.value === "") {
-      document.getElementById("errorMessage").innerHTML = "The task is empty"
-      event.value = currentTask;
-      return;
-    }
-
-    else
-    {
-      document.getElementById("errorMessage").innerHTML = "";
-    }
-
-    // if task already exist
-    TASKS.forEach(task => {
-      if (task.task === event.value) {
-      document.getElementById("errorMessage").innerHTML = "The task already exist!";
-        event.value = currentTask;
-        return;
-      }
-
-      else{
-      document.getElementById("errorMessage").innerHTML = "";
-      }
-
-    })
-    
-    // update task
-    TASKS.forEach(task => {
-      if (task.task === currentTask) {
-        task.task = event.value;
-      }
-      
-    })
-
-    // update local storage
-    localStorage.setItem("tasks", JSON.stringify(TASKS));
+const deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+  task.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("tasks", JSON.stringify(task));
+  console.log(task);
+  
 };
 
 
+//edit tasks
 
-/* ---- help messages display when task is added or del */
+// this creates a new task and saves the new value then deletes the old one
+const editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
 
-const taskAddedModal = document.querySelector("#taskAddMessage");
+  taskInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  taskDescription.value = selectedTask.children[2].innerHTML;
 
-const displayTaskAddedModal = () => {
-  taskAddedModal.style.display = "block";
-};
-
-const hideTaskAddedModal = () => {
-  taskAddedModal.style.display = "none";
-};
-
-const taskDeletedModal = document.querySelector("#delTaskMessage");
-
-const displayTaskDeletedModal = () => {
-  taskDeletedModal.style.display = "block";
-};
-
-const hideTaskDeletedModal = () => {
-  taskDeletedModal.style.display = "none";
+  deleteTask(e);
 };
 
 
-/* ---- event listeners ---- */
-MODAL_BTN.addEventListener('click', displayModal);
-CANCEL_BTN.addEventListener('click', hideModal);
+//reset form to default
 
-document.querySelector("#addBtn").addEventListener("click", displayTaskAddedModal);
-document.querySelector("#taskAddMessage").addEventListener("click", hideTaskAddedModal);
-
-
-document.querySelector("#addBtn").addEventListener("click", addTask);
-document.querySelector("#addBtn").addEventListener("click", hideModal);
+const resetForm = () => {
+  taskInput.value = "";
+  dateInput.value = "";
+  taskDescription.value = "";
+};
 
 
-document.querySelector("#delBtn").addEventListener("click", displayTaskDeletedModal);
-document.querySelector("#delTaskMessage").addEventListener("click", hideTaskDeletedModal);
-
-
-/*
- ----- the naming conventions used -----
-
-    -camelCase = for normal variable and function declaration
-    -SCREAMING_SNAKE_CASE = for global variable and function declaration
-
-*/
+/* ---- local storage ---- */
+(() => {
+  task = JSON.parse(localStorage.getItem("tasks")) || []
+  console.log(task);
+  createTasks();
+})();
